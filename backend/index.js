d6ea08db4664df5874fd9f1809b0ef4eb04a965d
Shubham <144575875+Shumbham-Gupta@users.noreply.github.com>
2025-10-27 16:40:32 +0530

    Update CORS origin to production URL

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ import geminiResponse from './gemini.js';
 
 const app = express();
 app.use(cors({
-  origin: 'http://localhost:5173', // Replace with your frontend URL
+  origin: 'https://virtualassistant-frontend-c2hv.onrender.com', // Replace with your frontend URL
   credentials: true, // Allow cookies to be sent
 }));
 const PORT = process.env.PORT || 5000; // Default to 5000 if PORT is not set
